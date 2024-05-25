3c82c5f526442fa285e39930195596bb01d11c0e
vinay.kumar <vinay.kumar@zohocorp.com>
2024-05-25 10:25:05 +0530

    pip

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,5 @@ echo "BUILD START"
 python3.9 -m ensurepip
 python3.9 -m pip install -r requirements.txt
 python3.9 -m pip install psycopg2-binary
-python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput
 echo "BUILD END"
