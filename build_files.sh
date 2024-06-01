ce5b165c463e0528cb92feeb876a2d3ff33f7d2a
Vinay Kumar V C <84114504+VINUVARDHAN@users.noreply.github.com>
2024-06-01 09:33:31 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,24 @@
+# echo "BUILD START"
+# python3.9 -m ensurepip --upgrade
+# python3.9 -m pip install --upgrade pip
+# python3.9 -m pip install setuptools
+# python3.9 -m pip install -r requirements.txt
+# python3.9 -m pip install psycopg2-binary
+# python3.9 manage.py collectstatic --noinput
+# echo "BUILD END"
+
 echo "BUILD START"
+
+python3.9 -m venv venv
+source venv/bin/activate
+
+# Upgrade pip and install dependencies
 python3.9 -m ensurepip --upgrade
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install setuptools
+python3.9 -m pip install --upgrade pip setuptools
 python3.9 -m pip install -r requirements.txt
 python3.9 -m pip install psycopg2-binary
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput
+
 echo "BUILD END"
